d67d2f043441d60ad4b57d203aced77d410efc58
Administrator <Administrator@PC-12>
2025-10-24 17:51:08 +0330

    Change on login and work with files in csharp

--- ProgrammingInCSharp0407.Panel/Form1.cs
+++ ProgrammingInCSharp0407.Panel/Form1.cs
@@ -1,4 +1,7 @@
+using Newtonsoft.Json;
 using ProgrammingInCSharp0407.Panel.Forms;
+using ProgrammingInCSharp0407.Panel.Wrappers;
+using System;
 
 namespace ProgrammingInCSharp0407.Panel
 {
@@ -25,16 +28,61 @@ namespace ProgrammingInCSharp0407.Panel
             //file check
 
             //hard code
-            if (username == "admin" && password == "1234")
+
+            string pathUserLoginFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "UserLogin.json");
+            if (!File.Exists(pathUserLoginFile))
+            {
+                MessageBox.Show("Invaid File Path");
+                return;
+            }
+
+            string content = File.ReadAllText(pathUserLoginFile);
+            List<UserLogin> userLogins = JsonConvert.DeserializeObject<List<UserLogin>>(content);
+
+            var loing = userLogins.Where(item => item.UserName == username && item.Password == password).FirstOrDefault();
+            if (loing is not null)
             {
                 LayoutPanelForm form = new LayoutPanelForm();
                 form.ShowDialog();
-            }
-            else
-            {
-                MessageBox.Show("Invalid Login");
+                return;
             }
 
+            //for (int i = 0; i < userLogins.Count; i++)
+            //{
+            //    if (username == userLogins[i].UserName && password == userLogins[i].Password)
+            //    {
+            //        LayoutPanelForm form = new LayoutPanelForm();
+            //        form.ShowDialog();
+            //        return;
+            //    }
+            //}
+
+            //string pathUserLoginFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "UserLogin.txt");
+            //if (!File.Exists(pathUserLoginFile))
+            //{
+            //    MessageBox.Show("Invaid File Path");
+            //    return;
+            //}
+
+            //string[] lines = File.ReadAllLines(pathUserLoginFile);
+            //for (int i = 0; i < lines.Length; i++)
+            //{
+            //    string currentLine = lines[i];//admin,1234
+            //    string[] login = currentLine.Split(",");
+
+            //    //Work with Files
+            //    //Json File
+            //    //Install Package & Usage Package
+            //    if (username == login[0] && password == login[1])
+            //    {
+            //        LayoutPanelForm form = new LayoutPanelForm();
+            //        form.ShowDialog();
+            //        return;
+            //    }
+            //}
+
+
+            MessageBox.Show("Invalid Login");
         }
 
     }
---
+++ ProgrammingInCSharp0407.Panel/Wrappers/UserLogin.cs
@@ -0,0 +1,7 @@
+﻿namespace ProgrammingInCSharp0407.Panel.Wrappers;
+
+internal sealed class UserLogin
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
